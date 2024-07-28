8d9bbe69b6e5b79d9fb1bfc7afc8d58dad013421
Martijn Kuik <77934777+martijnkuik@users.noreply.github.com>
2024-07-28 14:08:49 +0200

    Create palindrome.js

---
+++ webdev/codedex/js/palindrome.js
@@ -0,0 +1,13 @@
+function isPalindrome(word) {
+  let reversedWord = "";
+  
+  let lowerCaseWord = word.toLowerCase();
+  
+  for (let i = lowerCaseWord.length - 1; i >= 0; i--) {
+    reversedWord += lowerCaseWord[i];
+  }
+  
+  return reversedWord == lowerCaseWord;
+}
+
+console.log(isPalindrome("moonlight", "racecar"));
