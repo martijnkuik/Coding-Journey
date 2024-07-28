d70a7676ca5542123219e638b77016d3ffb8aeac
Martijn Kuik <77934777+martijnkuik@users.noreply.github.com>
2024-07-28 14:04:42 +0200

    Create music playlist.js

---
+++ webdev/codedex/js/music playlist.js
@@ -0,0 +1,20 @@
+const musicPlaylist = [
+  "Tom Sawyer",
+  "Sabotage",
+  "I Wanna Dance With Somebody",
+  "Don't Speak",
+  "Bulls On Parade",
+  "Thriller",
+  "The Breaks",
+  "Brick",
+  "Aeroplane Over the Sea",
+  "Tubthumping"
+];
+
+musicPlaylist.shift("Tom Sawyer");
+musicPlaylist.pop("Tubthumping" );
+musicPlaylist.push("This is the end")
+musicPlaylist.unshift("Joweh", "Mutwat");
+
+
+console.log(musicPlaylist)
